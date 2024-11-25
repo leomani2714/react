26034268bf998bb98ae32030769d2703349d28df
Mani <kandanmani244@gmail.com>
2024-11-25 07:41:12 +0530

    third commit

--- react.js
+++ react.js
@@ -1,5 +1,9 @@
-function MyButton() {
-  return (
-    <button>I'm a button</button>
-  );
+function sum(a) {
+  let total = 0; // Use a clear variable name like 'total'
+  for (let i = 0; i < a.length; i++) { // Correct the loop condition
+    total += a[i]; // Accumulate the values
+  }
+  return total; // Return the total sum
 }
+
+console.log(sum([0, 1, 2])); // Correctly log the result
